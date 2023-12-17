1f2ddfb30740b50f679de0318988c4e91762e1e7
ArcSoftwareX <arc.software.x@gmail.com>
2023-12-17 16:07:56 +0300

    chore: vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import { dirname, join as pathJoin } from "path";
 import { fileURLToPath } from "url";
 import tailwind from "@astrojs/tailwind";
-
 import qwikdev from "@qwikdev/astro";
+import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +20,6 @@ export default defineConfig({
   prefetch: {
     defaultStrategy: "hover"
   },
-  integrations: [tailwind(), qwikdev()]
+  integrations: [tailwind(), qwikdev()],
+  adapter: vercel()
 });
